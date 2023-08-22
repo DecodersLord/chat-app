56710d6ce4c67b784c13432998a4ffdc4112b440
Priyank Sevak <priyank.sevak1996@gmail.com>
2023-08-22 00:13:48 -0400

    Setting up users and beautify UI

--- server.js
+++ server.js
@@ -5,14 +5,33 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
+const users = [];
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
+  socket.on('user joined', (username) => {
+    // Store the username in the socket object
+    socket.username = username;
+    
+    // Broadcast a system message indicating that a user has joined
+    io.emit('chat message', {
+      text: `${username} has joined the chat.`,
+      system: true
+    });
+
+    if(users.indexOf(username) === -1){
+      users.push(username);
+      socket.username = username;
+      io.emit('update userList', users);
+      console.log(users);
+    }
+  });
   console.log('A user connected');
+  
+ 
 
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    io.emit('chat message', msg); // Broadcast the message to all connected clients
   });
 
   socket.on('disconnect', () => {
